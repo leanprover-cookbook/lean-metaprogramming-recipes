634424e02ce1af9f5efda5437d70a206a6aa1b04
Anirudh Gupta <anirudhgupta@iisc.ac.in>
2026-03-11 15:49:00 +0530

    shift concat to miscellaneous

--- Cookbook/FileSystem/Miscellaneous.lean
+++ Cookbook/FileSystem/Miscellaneous.lean
@@ -11,6 +11,27 @@ set_option pp.rawOnError true
 
 #doc (Manual) "Miscellaneous FileSystem Usages" =>
 
+# How to concatenate file paths
+
+%%%
+tag := "concatenating-file-paths"
+number := false
+%%%
+
+{index}[Concatenating file paths]
+
+To concatenate file paths, you can use the `System.FilePath` module. You can create a file path using `System.mkFilePath` and then concatenate it with another path using the `/` operator:
+
+```lean
+def concatPaths (base : System.FilePath) (sub : String) : System.FilePath :=
+  base / System.mkFilePath [sub]
+
+#eval concatPaths (System.mkFilePath ["home", "user"]) "documents"
+#eval System.mkFilePath ["home", "user"] / System.mkFilePath ["documents"]
+```
+
+This object you can use like usual since the new path is still a `System.FilePath` object.
+
 # How to get the current working directory
 
 %%%
--- Cookbook/FileSystem/readingFromFile.lean
+++ Cookbook/FileSystem/readingFromFile.lean
@@ -62,25 +62,3 @@ def readTrimmedLines (path : System.FilePath) :
   let line ← readFirstLine path
   return line.trimAscii.toString 
 ```
-
-# How to concatenate file paths
-
-%%%
-tag := "concatenating-file-paths"
-number := false
-%%%
-
-{index}[Concatenating file paths]
-
-To concatenate file paths, you can use the `System.FilePath` module. You can create a file path using `System.mkFilePath` and then concatenate it with another path using the `/` operator:
-
-```lean
-def concatPaths (base : System.FilePath) (sub : String) : System.FilePath :=
-  base / System.mkFilePath [sub]
-
-#eval concatPaths (System.mkFilePath ["home", "user"]) "documents"
-#eval System.mkFilePath ["home", "user"] / System.mkFilePath ["documents"]
-```
-
-This object you can read using the previous functions since the new path is still a `System.FilePath` object.
-
