bf23d0d7661e28218b7fdae45edd26847fc10a5e
Anirudh Gupta <anirudhgupta@iisc.ac.in>
2026-03-18 11:26:56 +0530

    toml init

--- Cookbook/DataStructures.lean
+++ Cookbook/DataStructures.lean
@@ -1,6 +1,7 @@
 import VersoManual
 import Cookbook.Lean
 import Cookbook.DataStructures.JSON
+import Cookbook.DataStructures.TOML
 
 open Verso.Genre Manual Cookbook
 open Verso.Genre.Manual.InlineLean
@@ -15,8 +16,10 @@ number := false
 ::: contributors
 :::
 
-Lean 4 provides several built-in data structures and tools for managing them, like JSON. This chapter deals with handling data structures with some custom examples of data structures and how to use them. 
+Lean 4 provides several built-in data structures and tools for managing them, like JSON and TOML. This chapter deals with handling data structures with some custom examples of data structures and how to use them. 
 
 *Note:* We will avoid covering basic operations on data structures like `Array` and `List` since they are fairly straightforward and multiple resources are available online for them.
 
 {include 1 Cookbook.DataStructures.JSON}
+
+{include 1 Cookbook.DataStructures.TOML}
