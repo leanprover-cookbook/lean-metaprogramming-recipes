bc1de2061599383e31c67675f548b0f0f2fb2150
ajay-k-nair <anir71@gmail.com>
2026-04-02 16:42:06 +0530

    rechanged folder name to Elaboration

---
+++ Cookbook/Elaboration.lean
@@ -0,0 +1,21 @@
+import VersoManual
+import Cookbook.Lean
+import Cookbook.Elaboration.ElaboratingTermsCommandsAndTactics
+import Cookbook.Elaboration.SyntaxForCommands
+import Cookbook.Elaboration.SyntaxForTerms
+
+open Verso.Genre Manual Cookbook
+
+#doc (Manual) "Elaboration: Extending Syntax" =>
+
+%%%
+tag := "elaboration-extending-syntax"
+number := false
+%%%
+
+::: contributors
+:::
+
+{include 1 Cookbook.Elaboration.ElaboratingTermsCommandsAndTactics}
+{include 1 Cookbook.Elaboration.SyntaxForCommands}
+{include 1 Cookbook.Elaboration.SyntaxForTerms}
