a54bf40f504bf87c655a4fa22ab8dffdb18e14be
ajay-k-nair <anir71@gmail.com>
2026-04-07 19:40:24 +0530

    improved Tactic recipes

--- Cookbook/Tactics/TacticUsingMacro.lean
+++ Cookbook/Tactics/TacticUsingMacro.lean
@@ -22,9 +22,13 @@ htmlSplit := .never
 
 {index}[Tactic using macros]
 
-# `repeat_apply` tactic
+We have seen how to write custom syntax and a `macro` that parses that syntax in the recipe {ref "writing-a-macro"}[*Writing a Macro*]. In this recipe, we will see how to use `macro` to write a custom tactic. We will start with a simple example of a tactic that applies the same theorem repeatedly until it can no longer be applied, and then we will generalize it to a tactic that takes two theorems as arguments and applies them in a specific order.
 
-We start with an example of a routine proof we might do. To prove `2 ≤ 6`.
+# Tactic to prove natural number inequalities
+{index}[Tactic to prove natural number inequalities]
+
+We start with an example of a routine proof we might write to prove `2 ≤ 6`.
+
 ```lean
 example : 2 ≤ 6 := by
   apply Nat.le_succ_of_le
@@ -33,21 +37,29 @@ example : 2 ≤ 6 := by
   apply Nat.le_succ_of_le
   apply Nat.le_refl
 ```
- This approach is highly repetitive. We can simplify it by using the `repeat` and `first` tactic combinators.
- ```lean
- example : 2 ≤ 6 := by
-  repeat (first| apply Nat.le_refl | apply Nat.le_succ_of_le)
- ```
+
+This approach is highly repetitive. We can simplify it by using the `repeat` and `first` tactic combinators.
+
+```lean
+example : 2 ≤ 6 := by
+  repeat (first| apply Nat.le_refl |
+  apply Nat.le_succ_of_le)
+```
+
 To reduce this to a single, readable line, we can define a custom tactic using a `macro`.
- ```lean
- macro "nat_le" : tactic =>
+
+```lean
+macro "nat_le" : tactic =>
   `(tactic| repeat(first| apply Nat.le_refl |
     apply Nat.le_succ_of_le))
 
- example: 2 ≤ 6 := by nat_le
- ```
+example: 2 ≤ 6 := by nat_le
+```
 
-While this works for our specific case, we can make it far more useful by abstracting the theorems. Let's construct a parameterized `macro` that accepts two theorems as arguments. It will repeatedly attempt to apply the second theorem (`t₂`) to close the goal, and whenever that fails, it makes progress by applying the first theorem (`t₁`).
+# Tactic to repeat applications of theorems
+{index}[Tactic to repeat applications of theorems]
+
+While `nat_le` works for our specific case, we can make it far more useful by abstracting over the theorems. Let's construct a parametrized `macro` that accepts two theorems as arguments. It will repeatedly attempt to apply the second theorem (`t₂`) to close the goal, and whenever that fails, it makes progress by applying the first theorem (`t₁`).
 
 Here is how we construct such a tactic:
 
@@ -55,6 +67,6 @@ Here is how we construct such a tactic:
 macro "repeat_apply" t₁:term "then" t₂:term : tactic  =>
     `(tactic| repeat(first| apply $t₂| apply $t₁ ))
 
-example : 10 ≤ 12 := by repeat_apply
-        Nat.le_succ_of_le then Nat.le_refl
+example : 10 ≤ 12 := by
+  repeat_apply Nat.le_succ_of_le then Nat.le_refl
 ```
