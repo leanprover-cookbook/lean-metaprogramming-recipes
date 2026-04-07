978b48276df37eb14b649c0bc90909d3fb9904e2
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2026-04-07 14:02:13 +0530

    building functions

---
+++ Cookbook/Expressions/FunctionExpressions.lean
@@ -0,0 +1,71 @@
+import VersoManual
+import Cookbook.Lean
+
+open Verso.Genre Manual Cookbook
+open Verso.Genre.Manual.InlineLean
+
+set_option pp.rawOnError true
+
+#doc (Manual) "Expressions for (dependent) Functions and Function-types" =>
+
+%%%
+tag := "expressions-for-functions"
+number := false
+-- Optional: If you don't want the recipe to be split into multiple subpages, because of depth.
+htmlSplit := .never
+%%%
+
+::: contributors
+:::
+
+{index}[Expressions for (dependent) Functions and Function-types]
+
+# Defining functions using λ-expressions
+
+Suppose we want to define an expression for the function that takes a natural number `n` and returns `n + n`. It is not advisable to directly build an expression using the constructor `Expr.lam` for lambda expressions, as Lean's hygeine modifies the expression. However, Lean provides convenient ways to introduce local variables and build lambda expressions using `withLocalDeclD` (or `withLocalDecl`) and `mkLambdaFVars`. Here is how we can define the expression for the doubling function:
+
+```lean
+open Lean Meta Elab
+
+def doubleExpr : MetaM Expr :=
+  withLocalDeclD `n (mkConst ``Nat) fun n => do
+    let double ← mkAppM ``Add.add #[n, n]
+    mkLambdaFVars #[n] double
+```
+
+The function `withLocalDeclD` has three arguments: the name of the local variable, its type (in this case `Nat`), and a continuation function that takes the newly created local variable as an argument. Inside the continuation, we can build the body of the lambda expression using `mkAppM` to apply the addition function to `n` and `n`. Finally, we use `mkLambdaFVars` to create a lambda expression that abstracts over the local variable `n`.
+
+To illustrate how to use this expression, we can write an elaborator (see {ref "elaborators"}[Elaborators]) that allows us to use it in term position:
+
+```lean
+elab "double%" : term =>
+  doubleExpr
+
+#eval double% 7 -- 14
+```
+
+# Defining dependent functions using Π-types
+
+We can use similar techniques to define expressions for dependent functions and `∀` quantified propositions, which are represented by Π-types in Lean. For example, we can define an expression for the proposition `forall n : Nat, n = n` as follows:
+
+```lean
+def rflNatExpr : MetaM Expr :=
+  withLocalDeclD `n (mkConst ``Nat) fun n => do
+    let eqn ← mkEq n n
+    mkForallFVars #[n] eqn
+
+elab "rflnat%" : term => do
+  rflNatExpr
+
+example : rflnat% := by -- goal `∀ (n : Nat), n = n`
+  simp
+```
+
+# Defining function types using `mkArrow`
+
+In case of non-dependent function types, we can use the `mkArrow` function to build expressions for function types. For example, we can define an expression for the type of functions from `Nat` to `Nat` as follows:
+
+```lean
+def natToNatExpr : MetaM Expr :=
+  mkArrow (mkConst ``Nat) (mkConst ``Nat)
+```
